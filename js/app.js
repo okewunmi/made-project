ed520516c5264c00de8fcf10bf5ba0ed7efa6fc8
okewunmi Afeez <okewunmiafeezolaide@mail.com>
2021-06-20 20:29:18 +0100

    stages

---
+++ js/app.js
@@ -0,0 +1,10 @@
+/* for the play button*/
+const btn = document.querySelector(".btn-play1");
+const video = document.querySelector(".main--img");
+
+btn.addEventListener ("click", function() {
+video.pause();
+}); 
+
+
+/************************* */
